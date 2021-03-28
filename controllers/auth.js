71f03ec8a102deae3fd65d1a57800af637ab1539
CarlosGRJ <carlosgrj@outlook.com>
2021-03-28 13:33:39 -0600

    Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import User from '../models/user';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
    console.log(req.body);
@@ -23,5 +24,37 @@ export const register = async (req, res) => {
    } catch (error) {
       console.log('CREATE USER FAILED', error);
       return res.status(400).send('Error. Try again');
+   }
+};
+
+export const login = async (req, res) => {
+   const { email, password } = req.body;
+
+   try {
+      // Check if user with that email exist
+      const user = await User.findOne({ email }).exec();
+      if (!user) res.status(400).send('User with that email not found');
+      // compare password
+      user.comparePassword(password, (err, match) => {
+         console.log('COMPARE PASSWORD IN LOGIN ERR', err);
+         if (!match || err) return res.status(400).send('Wrong password');
+         // GENERATE A TOKEN THEN SEND AS RESPONSE TO CLIENT
+         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+         });
+         res.json({
+            token,
+            user: {
+               _id: user._id,
+               name: user.name,
+               email: user.email,
+               createdAt: user.createdAt,
+               updatedAt: user.updatedAt,
+            },
+         });
+      });
+   } catch (error) {
+      console.log('LOGIN ERROR', error);
+      res.status(400).send('Signin failed');
    }
 };
