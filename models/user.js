dfe5763164fe27a293ae9b02a10a56e78403cc1b
CarlosGRJ <carlosgrj@outlook.com>
2021-03-27 18:45:21 -0600

    Bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -26,5 +27,21 @@ const userSchema = new Schema(
    },
    { timestamps: true },
 );
+
+userSchema.pre('save', function (next) {
+   let user = this;
+
+   if (user.isModified('password')) {
+      return bcrypt.hash(user.password, 12, function (err, hash) {
+         if (err) {
+            console.log('BCRYPT HASH ERR ', err);
+         }
+         user.password = hash;
+         return next();
+      });
+   } else {
+      return next();
+   }
+});
 
 export default mongoose.model('User', userSchema);
