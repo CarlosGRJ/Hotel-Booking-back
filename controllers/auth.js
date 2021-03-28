dfd36461db719dd86ccb89f04c67b7abd0987940
CarlosGRJ <carlosgrj@outlook.com>
2021-03-27 19:23:19 -0600

    Saving User

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,27 @@
-export const showMessage = (req, res) => {
-   res.status(200).send(`Here is your message:  ${req.params.message}`);
-};
+import User from '../models/user';
 
 export const register = async (req, res) => {
    console.log(req.body);
-}
+   const { name, email, password } = req.body;
+   // Validation
+   if (!name) return res.status(400).send('Name is required');
+   if (!password || password.length < 6)
+      return res
+         .status(400)
+         .send('Password is required and should be min 6 characters long');
+
+   const userExist = await User.findOne({ email }).exec();
+   if (userExist) return res.status(400).send('Email is taken');
+
+   // register
+   const user = new User(req.body);
+
+   try {
+      await user.save();
+      console.log('USER CREATED', user);
+      return res.json({ ok: true });
+   } catch (error) {
+      console.log('CREATE USER FAILED', error);
+      return res.status(400).send('Error. Try again');
+   }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 // controllers
-import { showMessage, register } from '../controllers/auth';
+import { register } from '../controllers/auth';
 
-router.get('/:message', showMessage);
 router.post('/register', register);
 
 module.exports = router;
